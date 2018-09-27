0933fffba1a2f227be11f58e97a25d4bb43ac4f6
Leonardo Amorim <leonardocutrimamorim@gmail.com>
2018-09-27 11:06:13 +0200

    To test it

--- verifvalid/ViewController.swift
+++ verifvalid/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(wordCount(s: "foo foo bar"))
+        print(getContentsFromFile())
+        print(wordCountFile(path: "/Users/leonardoamorim/Desktop/verifvalid/teste.txt"))
     }
 
 
--- verifvalid/wordcount.swift
+++ verifvalid/wordcount.swift
@@ -8,8 +8,43 @@
 
 import Foundation
 
-func wordCount(s: String) -> Dictionary<String, Int> {
+
+
+func wordCountFile(path: String) -> Dictionary<String, Int> {
     
+    var wordDictionary = Dictionary<String, Int>()
+    do {
+        
+        let contents = try NSString(contentsOfFile: path, encoding: String.Encoding.utf8.rawValue)
+            .components(separatedBy: .punctuationCharacters)
+            .joined()
+            .components(separatedBy: .whitespacesAndNewlines)
+        
+        for word in contents {
+            if let count = wordDictionary[word] {
+                wordDictionary[word] = count + 1
+            } else {
+                wordDictionary[word] = 1
+            }
+        }
+    } catch _ as NSError {
+        print("Something is wrong")
+    }
+    return wordDictionary
+}
+
+func getContentsFromFile() {
+    let path = "/Users/leonardoamorim/Desktop/verifvalid/teste.txt"
+    
+    do {
+        let contents = try NSString(contentsOfFile: path, encoding: String.Encoding.utf8.rawValue)
+        print(contents)
+    } catch _ as NSError {
+        print("Opps.Something is wrong")
+    }
+}
+
+func wordCount(s: String) -> Dictionary<String, Int> {
     
     var words = s.components(separatedBy: NSCharacterSet.whitespacesAndNewlines)
     var wordDictionary = Dictionary<String, Int>()
